f61db9b1dc398b36de531109c90d87b3bc3fae83
parisvennis <parisvennis@gmail.com>
2020-07-23 19:49:52 +0000

    initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,29 @@
-/* eslint-disable */
+var battlefield = document.querySelector(".container");
+const fire = () => {
+  battlefield.childNodes[Math.floor(Math.random() * 10)].childNodes[
+    Math.floor(Math.random() * 10)
+  ].style.background = "red";
+};
 
-import "../assets/img/rigo-baby.jpg";
-import "../assets/img/4geeks.ico";
-//import 'breathecode-dom'; //DOM override to make JS easier to use
-import "../style/index.scss";
+window.onload = () => {
+  for (var i = 0; i < 10; i++) {
+    var row = document.createElement("div");
+    battlefield.appendChild(row);
+    row.style.display = "flex";
+    for (var k = 0; k < 10; k++) {
+      var col = document.createElement("div");
+      row.appendChild(col);
+      col.innerHTML = Math.floor(Math.random() * 2);
+      col.style.border = "2px solid gray";
+      col.style.padding = "15px";
+      if (col.innerHTML == 1) col.style.background = "gray";
+    }
+  }
+  var button = document.createElement("button");
+  button.innerHTML = "!FIRE!";
+  button.style.padding = "10px 30px";
+  button.style.background = "red";
+  battlefield.appendChild(button);
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  button.addEventListener("click", fire);
 };
